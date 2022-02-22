6970e9a8329350518b10c4e5ab680509725e2bd4
Joserra <jrf.nisu@gmail.com>
2022-02-22 23:26:50 +0100

    Removed /**

--- src/main/java/com/osp/codechallenge/configuration/SecurityConfig.java
+++ src/main/java/com/osp/codechallenge/configuration/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/swagger-ui/**", "/openapi/**", "/**").permitAll()
+                .antMatchers("/swagger-ui/**", "/openapi/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
