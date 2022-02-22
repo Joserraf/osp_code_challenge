dea849e3d919c0d73ec00382fcdfa0740ce6a837
Joserra <jrf.nisu@gmail.com>
2022-02-22 16:24:45 +0100

    Added not empty constraint

--- src/main/java/com/osp/codechallenge/documents/TrackingKey.java
+++ src/main/java/com/osp/codechallenge/documents/TrackingKey.java
@@ -2,14 +2,17 @@ package com.osp.codechallenge.documents;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
 public class TrackingKey {
 
     @NotNull
+    @NotEmpty
     private String carrier;
 
     @NotNull
+    @NotEmpty
     private String trackingNumber;
 }
